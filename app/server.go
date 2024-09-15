b0dffb7812edb1a0526bcc79afc42c599d8c779d
Segun <folowosele.segun@gmail.com>
2024-09-15 15:56:56 +0400

    serial requests

--- app/server.go
+++ app/server.go
@@ -29,60 +29,79 @@ func main() {
 		conn, err := l.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection:", err)
-			os.Exit(1)
-		}
-
-		buffer := make([]byte, 1024)
-		n, err := io.ReadFull(conn, buffer[:12]) // Read at least 12 bytes to get correlationID
-		if err != nil {
-			fmt.Println("Error reading from connection:", err)
-			conn.Close()
 			continue
 		}
-		fmt.Printf("Read %d bytes from connection\n", n)
+		go handleConnection(conn)
+	}
+}
 
-		apiVersion := binary.BigEndian.Uint16(buffer[4+2 : 4+2+2])
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	for {
+		var lengthBuf [4]byte
+		_, err := io.ReadFull(conn, lengthBuf[:])
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println("Error reading length from connection:", err)
+			}
+			return
+		}
+		messageLength := binary.BigEndian.Uint32(lengthBuf[:])
+		fmt.Printf("Received message of length: %d\n", messageLength)
+
+		messageBuf := make([]byte, messageLength)
+		_, err = io.ReadFull(conn, messageBuf)
+		if err != nil {
+			fmt.Println("Error reading message from connection:", err)
+			return
+		}
+
+		if len(messageBuf) < 8 {
+			fmt.Println("Message too short")
+			continue
+		}
+
+		apiKey := binary.BigEndian.Uint16(messageBuf[0:2])
+		apiVersion := binary.BigEndian.Uint16(messageBuf[2:4])
+		fmt.Printf("Parsed API Key: %d\n", apiKey)
 		fmt.Printf("Parsed API Version: %d\n", apiVersion)
 
-		correlationID := binary.BigEndian.Uint32(buffer[4+2+2 : 4+2+2+4]) // length (4) + api_key(2) + api_version(2)... correlationID is 4 byte
+		correlationID := binary.BigEndian.Uint32(messageBuf[4:8])
 		fmt.Printf("Parsed correlation ID: %d\n", correlationID)
 
-		response := []byte{0x00, 0x00, 0x00, 0x13}
+		response := make([]byte, 4)
 
 		corrId := make([]byte, 4)
-		binary.BigEndian.PutUint32(corrId, uint32(correlationID))
+		binary.BigEndian.PutUint32(corrId, correlationID)
 		response = append(response, corrId...)
 
 		errCode := make([]byte, 2)
 		if apiVersion != 0 && apiVersion != 1 && apiVersion != 2 && apiVersion != 3 && apiVersion != 4 {
-			fmt.Println("Invalid API Version, sending UNSUPPORTED_VERSION")
+			fmt.Println("Invalid API Version, sending UNKNOWN_VERSION")
 			binary.BigEndian.PutUint16(errCode, uint16(UNKNOWN_VERSION))
 		} else {
 			binary.BigEndian.PutUint16(errCode, uint16(NO_ERROR))
 		}
 		response = append(response, errCode...)
 
-		response = append(response, 0x02)       // api versions count
-		response = append(response, 0x00, 0x12) // api version = 18
-		response = append(response, 0x00, 0x04)
-		response = append(response, 0x00, 0x04)
-		response = append(response, 0x00)
-		response = append(response, 0x00, 0x00, 0x00, 0x00)
-		response = append(response, 0x00)
+		response = append(response, 0x02)                   // API versions count
+		response = append(response, 0x00, 0x12)             // API key = 18
+		response = append(response, 0x00, 0x00)             // Min API version
+		response = append(response, 0x00, 0x04)             // Max API version
+		response = append(response, 0x00)                   // TAG_BUFFER
+		response = append(response, 0x00, 0x00, 0x00, 0x00) // Throttle time (ms)
+		response = append(response, 0x00)                   // TAG_BUFFER
 
-		fmt.Printf("===========%v\n", response)
+		responseLength := uint32(len(response) - 4)
+		binary.BigEndian.PutUint32(response[0:4], responseLength)
+
+		fmt.Printf("Sending response of length %d: %v\n", responseLength, response)
+
 		_, err = conn.Write(response)
 		if err != nil {
 			fmt.Println("Error writing response:", err)
+			return
 		}
-
-		if tcpConn, ok := conn.(*net.TCPConn); ok {
-			err = tcpConn.CloseWrite()
-			if err != nil {
-				fmt.Println("Error closing write side of the connection:", err)
-			}
-		}
-
-		conn.Close()
 	}
 }
