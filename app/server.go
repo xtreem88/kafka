0b1584bf6200ceb0d21e9f1588498d08c784ac5b
Segun <folowosele.segun@gmail.com>
2024-09-15 09:49:07 +0400

    listen to port 9092

--- app/server.go
+++ app/server.go
@@ -16,14 +16,14 @@ func main() {
 
 	// Uncomment this block to pass the first stage
 	//
-	// l, err := net.Listen("tcp", "0.0.0.0:9092")
-	// if err != nil {
-	// 	fmt.Println("Failed to bind to port 9092")
-	// 	os.Exit(1)
-	// }
-	// _, err = l.Accept()
-	// if err != nil {
-	// 	fmt.Println("Error accepting connection: ", err.Error())
-	// 	os.Exit(1)
-	// }
+	l, err := net.Listen("tcp", "0.0.0.0:9092")
+	if err != nil {
+		fmt.Println("Failed to bind to port 9092")
+		os.Exit(1)
+	}
+	_, err = l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
 }
