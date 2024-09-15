d635bfd119876112990b5644721c819ef6df924b
Segun <folowosele.segun@gmail.com>
2024-09-15 10:26:44 +0400

    Send Correlation ID

--- app/server.go
+++ app/server.go
@@ -1,29 +1,49 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
+	"io"
 	"net"
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
-
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	fmt.Println("Starting Kafka server...")
 
-	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:9092")
 	if err != nil {
-		fmt.Println("Failed to bind to port 9092")
+		fmt.Println("Failed to bind to port 9092:", err)
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	defer l.Close()
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			os.Exit(1)
+		}
+
+		buffer := make([]byte, 1024)
+		_, err = conn.Read(buffer)
+		if err != nil && err != io.EOF {
+			fmt.Println("Error reading from connection:", err)
+			conn.Close()
+			continue
+		}
+
+		response := make([]byte, 8)
+
+		binary.BigEndian.PutUint32(response[0:4], 4)
+
+		binary.BigEndian.PutUint32(response[4:8], 7)
+
+		_, err = conn.Write(response)
+		if err != nil {
+			fmt.Println("Error writing response:", err)
+		}
+
+		conn.Close()
 	}
 }
