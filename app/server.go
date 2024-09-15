974091a44ad79acd9a27576d0a03d0310532e224
Segun <folowosele.segun@gmail.com>
2024-09-15 11:09:20 +0400

    read correlationID

--- app/server.go
+++ app/server.go
@@ -26,24 +26,36 @@ func main() {
 		}
 
 		buffer := make([]byte, 1024)
-		_, err = conn.Read(buffer)
-		if err != nil && err != io.EOF {
+		n, err := io.ReadFull(conn, buffer[:12]) // Read at least 12 bytes to get correlationID
+		if err != nil {
 			fmt.Println("Error reading from connection:", err)
 			conn.Close()
 			continue
 		}
+		fmt.Printf("Read %d bytes from connection\n", n)
+
+		correlationID := binary.BigEndian.Uint32(buffer[4+2+2 : 4+2+2+4]) // length (4) + api_key(2) + api_version(2)... correlationID is 4 byte
+		fmt.Printf("Parsed correlation ID: %d\n", correlationID)
 
 		response := make([]byte, 8)
 
 		binary.BigEndian.PutUint32(response[0:4], 4)
 
-		binary.BigEndian.PutUint32(response[4:8], 7)
+		binary.BigEndian.PutUint32(response[4:8], correlationID)
 
 		_, err = conn.Write(response)
 		if err != nil {
 			fmt.Println("Error writing response:", err)
 		}
 
-		conn.Close()
+		if tcpConn, ok := conn.(*net.TCPConn); ok {
+			err = tcpConn.CloseWrite()
+			if err != nil {
+				fmt.Println("Error closing write side of the connection:", err)
+			}
+		}
+
+		// We keep the read side of the connection open to avoid premature reset.
+		// The connection will eventually be closed when the client is done.
 	}
 }
