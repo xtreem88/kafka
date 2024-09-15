3c995da0bbb375a357c393f68f37dfe8418f91d1
Segun <folowosele.segun@gmail.com>
2024-09-15 12:27:35 +0400

    api version handling

--- app/server.go
+++ app/server.go
@@ -8,6 +8,13 @@ import (
 	"os"
 )
 
+type errorCode int16
+
+const (
+	NO_ERROR        errorCode = 0
+	UNKNOWN_VERSION errorCode = 35
+)
+
 func main() {
 	fmt.Println("Starting Kafka server...")
 
@@ -40,18 +47,30 @@ func main() {
 		correlationID := binary.BigEndian.Uint32(buffer[4+2+2 : 4+2+2+4]) // length (4) + api_key(2) + api_version(2)... correlationID is 4 byte
 		fmt.Printf("Parsed correlation ID: %d\n", correlationID)
 
-		response := make([]byte, 10)
+		response := []byte{0x00, 0x00, 0x00, 0x13}
 
-		binary.BigEndian.PutUint32(response[0:4], 4)
+		corrId := make([]byte, 4)
+		binary.BigEndian.PutUint32(corrId, uint32(correlationID))
+		response = append(response, corrId...)
 
-		binary.BigEndian.PutUint32(response[4:8], correlationID)
-
+		errCode := make([]byte, 2)
 		if apiVersion != 0 && apiVersion != 1 && apiVersion != 2 && apiVersion != 3 && apiVersion != 4 {
 			fmt.Println("Invalid API Version, sending UNSUPPORTED_VERSION")
-			binary.BigEndian.PutUint16(response[8:10], 35)
-			fmt.Printf("===========%v\n", response)
+			binary.BigEndian.PutUint16(errCode, uint16(UNKNOWN_VERSION))
+		} else {
+			binary.BigEndian.PutUint16(errCode, uint16(NO_ERROR))
 		}
+		response = append(response, errCode...)
 
+		response = append(response, 0x02)       // api versions count
+		response = append(response, 0x00, 0x12) // api version = 18
+		response = append(response, 0x00, 0x04)
+		response = append(response, 0x00, 0x04)
+		response = append(response, 0x00)
+		response = append(response, 0x00, 0x00, 0x00, 0x00)
+		response = append(response, 0x00)
+
+		fmt.Printf("===========%v\n", response)
 		_, err = conn.Write(response)
 		if err != nil {
 			fmt.Println("Error writing response:", err)
